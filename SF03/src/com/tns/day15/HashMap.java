27750027fbceea54d29a7241a7224239e0c1768a
AB Tech <AB Tech@DESKTOP-66BO4TM>
2024-07-03 20:46:47 +0530

    commit

---
+++ SF03/src/com/tns/day15/HashMap.java
@@ -0,0 +1,13 @@
+package com.tns.day15;
+/*
+ 	MAP---
+ 	put() - add elements
+ 	entrySet() -  iterate a map
+* keySet() - to get the key values only
+* Values() - to get the values only
+* clear() - removed all elements or mappings from a specified map
+* isEmpty() - return false/true
+* putAll() - 
+ */
+
+
